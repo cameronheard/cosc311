7c26190a60b3c0bed0fc39ac1e0ce2b834d1074f
Cameron Heard <cameron.heard@ithaka.org>
2018-09-18 14:34:49 -0400

    hw0911 - LinkedList with summation, average, and node deletion

--- src/HW0911/LinkedList.java
+++ src/HW0911/LinkedList.java
@@ -1,69 +1,97 @@
+/**
+ * Author:      Cameron Heard
+ * Course #:    COSC 311
+ * Semester:    Fall 2018
+ * Assignment:  hw0911
+ *
+ * HW0911
+ * LinkedList
+ * Node
+ */
 package HW0911;
 
+/* The basic structure for of this is from the LinkedList class I made for
+** COSC-211.
+*/
 class LinkedList {
 
-    private Node head;
-
-    public Node getHead(){
-        return head;
-    }
-
-    public Node getNext(){
-        return head.getNext();
-    }
-
-    public int getData(){
-        return head.getData();
-    }
-
-    public void setHead(){
-        head = head.getNext();
-    }
-
+    public Node head;
 
     public LinkedList(){
         head = null;
     }
 
-    public LinkedList(int[] arr){
-        for(int item : arr){
-            head = null;
+    public LinkedList(double[] arr){
+        head = null;
+        for(double item : arr){
             addLast(item);
         }
     }
 
-    public void addFirst(int num){
+    public void addFirst(double num){
         head = new Node(num, head);
     }
 
-    public void addLast(int num){
+    public void addLast(double num){
         if(head == null)
             addFirst(num);
         else{
             Node tmp = head;
-            while(tmp.link != null)
-                tmp = tmp.link;
-            tmp.link = new Node(num, null);
+            while(tmp.getNext() != null)
+                tmp = tmp.getNext();
+            tmp.setNext(new Node(num, null));
         }
     }
 
-    private class Node{
-        private int data;
-        private Node link;
-
-        public Node(int num, Node n){
-            data = num;
-            link = n;
+    public double sumNodes(){
+        double total = 0.0;
+        Node tmp = head;
+        do{
+            total = total + tmp.getData();
+            if(tmp.getNext() == null)
+                return total;
+            else{
+                tmp = tmp.getNext();
+            }
         }
-
-        public Node getNext(){
-            return this.link;
-        }
-
-        public int getData(){
-            return this.data;
-        }
+        while(true);
     }
 
+    public String toString(){
+        Node node = head;
+        String string = "";
+        do{
+            string = string + " " + node.getData();
+            if(node.getNext() == null)
+                return string;
+            else{
+                node = node.getNext();
+            }
+        }
+        while(true);
+    }
 
+    /* This is the delete method from your shared example, modified to allow for
+    ** continued traversal even after a match. Also, calling methods defined in
+    ** my Node class.
+     */
+    public void deleteAbove(double num) {
+
+        if (head == null) {
+            return;
+        }
+
+        if (head.getData() > num) {
+            head = head.getNext();
+        }
+
+        Node p, q;
+        for (p = head.getNext(), q = head; p != null; p = p.getNext(), q = q.getNext()) {
+            if (p.getData() > num) {
+                q.setNext(p.getNext());
+                if(q.getNext() != null)
+                    p = q.getNext();
+            }
+        }
+    }
 }
